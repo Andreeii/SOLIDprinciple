a4a1542923cc5b4dce14246e163c1c3ce3ee778b
Andrei Turcanu <Andrei Turcanu@DESKTOP-B0LQIU9>
2020-02-09 19:55:22 +0200

    -

--- SOLIDprinciple/MainClass.cs
+++ SOLIDprinciple/MainClass.cs
@@ -7,11 +7,7 @@ namespace SOLIDprinciple
         static void Main(string[] args)
         {
             String t = "afasdfsdaf";
-<<<<<<< HEAD
-            Iphone nokia = new Iphone();
-=======
             Iphone iphone5 = new Iphone();
->>>>>>> a3b4ebccbbe85cec80d557a599d8b0e44e47f839
 
             TeslaCar tesla = new TeslaCar();
 
